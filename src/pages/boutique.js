6ca678175a72be648feead1b806780ecd0ccd98c
Arnaud S <arnaud.sebaa@epitech.eu>
2019-10-16 14:45:41 +0200

    rewrite all

--- src/pages/boutique.js
+++ src/pages/boutique.js
@@ -1,72 +1,69 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
-import SEO from "../components/seo"
-import Boutique2 from "../components/boutique/boutique2"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+import { Card, Button, CardGroup, Col, Row } from "react-bootstrap"
 
+class Boutique extends React.Component {
+  	render() {
+		const { data } = this.props
+		const postList = data.allMarkdownRemark;
+		return (
+			<Layout>
+				<CardGroup class="d-flex p-3 bg-secondary lign-content-start flex-wrap justify-content-around">
+					{postList.edges.map(({ node }, i) => (
+						<Card class="card mb-3" style={{maxWidth: 600, width:600, marginTop: 30, marginRight: 30}} to={"shop/" + node.fields.slug} key={i} className="link">
+							<Row>
+								<Col sm={6}>
+									<Card.Img src={node.frontmatter.gallery} />
+								</Col>
+								<Col>
+									<Card.Body>
+										<Card.Text className="text-center" style={{fontSize: "30px"}}>
+											{node.frontmatter.name}
+										</Card.Text>
+										<Card.Text className="text-center mt-5 mb-5" style={{fontSize: "30px"}}>
+											{node.frontmatter.price + " €"}
+										</Card.Text>
+										<Button href={"shop" + node.fields.slug} className="mr-3" style={{fontSize: "25px"}} variant="primary">Détaille</Button>
+										<Button style={{fontSize: "25px"}} variant="danger">Acheter</Button>
+									</Card.Body>
+								</Col>
+							</Row>
 
-class Blog extends React.Component {
-  render() {
-    const { data } = this.props
-    const postList = data.allMarkdownRemark;
-
-    return (
-      <Boutique2>
-          <SEO title="Boutique" />
-          <div class="d-flex p-3 bg-secondary lign-content-start flex-wrap justify-content-around">
-            {postList.edges.map(({ node }, i) => (
-                <div class="card mb-3" style={{maxWidth: 600, width:600, marginTop: 30, marginRight: 30}}>
-                    <div class="row no-gutters">
-                      <div class="col-md-5">
-                        <img src={node.frontmatter.gallery} alt=""/>
-                      </div>
-                    <div class="col-md-6">
-                        <div class="card-body">
-                          <Link to={"shop/" + node.fields.slug} key={i} className="link" >
-                            <h5 class="card-title text-center" style={{fontSize: "30px"}}>{node.frontmatter.name} {node.frontmatter.id}</h5>
-                          </Link>
-                        </div>
-                        <div class="price text-center pt-2">
-                            <p style={{fontSize: "50px"}}>{node.frontmatter.price + " €"}</p>
-                            <Link to={"shop/" + node.fields.slug} key={i} className="link" >
-                              <div class="btn btn-lg btn-danger mt-2">Acheter <span class="glyphicon glyphicon-euro"></span></div>
-                            </Link>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-            ))}
-            </div>
-      </Boutique2>
-      )
+						</Card>
+					))}
+				</CardGroup>
+			</Layout>
+		)
     }
 }
 
-export default Blog
+export default Boutique
 
 export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
-          }
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            title
-            name
-            description
-            price
-            gallery
-          }
-        }
-      }
-    }
-  }
+	query {
+		site {
+			siteMetadata {
+				title
+			}
+		}
+		allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+			edges {
+				node {
+					excerpt
+					fields {
+						slug
+				}
+				frontmatter {
+					date(formatString: "MMMM DD, YYYY")
+					title
+					name
+					description
+					price
+					gallery
+				}
+			}
+		}
+		}
+	}
 `
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,46 @@
 import React from "react"
-import HomePage2 from "../components/homePage/homePage2"
-import SEO from "../components/seo"
-
+import Image from "gatsby-image"
+import { StaticQuery, graphql } from "gatsby"
+import Layout from "../components/layout"
+import styled from "styled-components"
 
 class IndexPage extends React.Component {
-  render() {
-    return (
-        <HomePage2>
-          <SEO
-          title="Home"
-          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
-        />
-        </HomePage2>     
-    )
-  }
+  	render() {
+    	return (
+        	<Layout>
+				<h1 class="text-center mb-5 font-weight-bold font-italic">GatsbyCommerce</h1><br/>
+				<StaticQuery
+					query={homePageQuery}
+					render={data => {
+						return (
+							<Container>
+								<Image fixed={data.homePage.childImageSharp.fixed} style={{ marginLeft: `100px`, marginRight: `100px`,}}/>
+								<p style={{fontSize: `30px`,}}>
+									Description
+								</p>
+							</Container>
+						)
+					}}
+				/>
+        	</Layout>     
+    	)
+  	}
 }
+
+const Container = styled.div`
+  display: flex;
+`
+
+const homePageQuery = graphql`
+	query homePage {
+		homePage: file(absolutePath: { regex: "/gatsby-icon.png/" }) {
+			childImageSharp {
+				fixed(width: 400, height: 400) {
+					...GatsbyImageSharpFixed
+				}
+			}
+		}
+	}
+`
 
 export default IndexPage
