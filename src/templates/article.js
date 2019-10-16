2f916111faccd31f624ed0e9647a6825d56284e6
Arnaud S <arnaud.sebaa@epitech.eu>
2019-10-16 17:14:42 +0200

    add homePage markdown, logo, description, name

--- src/templates/article.js
+++ src/templates/article.js
@@ -5,39 +5,39 @@ import Layout from "../components/layout"
 import { Card, Button, Col, Row } from "react-bootstrap"
 
 class Article extends React.Component {
-  render() {
-    const post = this.props.data.markdownRemark
-    return (
-      <Layout>
-			<Card class="card mb-3" style={{maxWidth: 800, width:800, marginTop: 30, marginRight: 30}}>
-				<Row>
-					<Col sm={6}>
-						<Card.Img src={post.frontmatter.gallery} />
-					</Col>
-					<Col>
-						<Card.Body>
-							<Card.Text className="text-center mb-2 font-weight-bold font-italic" style={{fontSize: "30px"}}>
-								{post.frontmatter.name}
-							</Card.Text>
-							<Card.Text className="text-center" style={{fontSize: "25px"}}>
-								{post.frontmatter.title}
-							</Card.Text>
-							<Card.Text className="text-center mt-5 mb-5" style={{fontSize: "38px"}}>
-								{post.frontmatter.price + " €"}
-							</Card.Text>
-							<Button style={{fontSize: "25px", alignSelf:'center'}} variant="danger">Acheter</Button>
-						</Card.Body>
-					</Col>
-				</Row>
-				<Card.Text class="col-md-12 text-center" style={{fontSize: "22px"}}>
-					{post.frontmatter.description}
-				</Card.Text>
-				<Card.Text class="text-center" style={{fontSize: "20px"}} dangerouslySetInnerHTML={{ __html: this.props.data.markdownRemark.html }} >
-				</Card.Text>
-			</Card>
-      </Layout>
-    )
-  }
+  	render() {
+		const post = this.props.data.markdownRemark
+		return (
+			<Layout>
+				<Card class="card mb-3" style={{maxWidth: 800, width:800, marginTop: 30, marginRight: 30, margin: "0 auto", float: "none",}}>
+					<Row>
+						<Col sm={6}>
+							<Card.Img src={post.frontmatter.gallery} />
+						</Col>
+						<Col>
+							<Card.Body>
+								<Card.Text className="text-center mb-2 font-weight-bold font-italic" style={{fontSize: "30px"}}>
+									{post.frontmatter.name}
+								</Card.Text>
+								<Card.Text className="text-center" style={{fontSize: "25px"}}>
+									{post.frontmatter.title}
+								</Card.Text>
+								<Card.Text className="text-center mt-5 mb-5" style={{fontSize: "38px"}}>
+									{post.frontmatter.price + " €"}
+								</Card.Text>
+								<Button style={{fontSize: "25px", display: "block", margin: "0 auto", float: "none",}} variant="danger">Acheter</Button>
+							</Card.Body>
+						</Col>
+					</Row>
+					<Card.Text class="col-md-12 text-center" style={{fontSize: "22px"}}>
+						{post.frontmatter.description}
+					</Card.Text>
+					<Card.Text class="text-center" style={{fontSize: "20px"}} dangerouslySetInnerHTML={{ __html: this.props.data.markdownRemark.html }} >
+					</Card.Text>
+				</Card>
+			</Layout>
+		)
+  	}
 }
 
 export default Article
